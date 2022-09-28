c7f97f73685e90f2170ab59413f14fe7dfc4eeb2
Efrain May <efrain_may_75@hotmail.com>
2022-09-28 12:13:00 -0500

    logica de los nombres de pokemones

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -2,19 +2,37 @@
   <div class="pokemon-container">
     <img
         class="hidden-pokemon"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        :src="imgSrc"
         alt="pokemon"
     />
     <img
+        v-if="showPokemon"
         class="fade-in"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        :src="imgSrc"
         alt="pokemon"
     />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        pokemonId: {
+            type: Number,
+            required: true
+        },
+        showPokemon: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    computed: {
+        imgSrc(){
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+        }
+    }
+};
 </script>
 
 <style scope>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,12 +1,16 @@
 <template>
     <h1>¿Quien es este Pokemon?</h1>
-    <PokemonPicture />
+    <PokemonPicture v-bind:pokemonId="150" v-bind:showPokemon="true" />
     <PokemonOptions />
 </template>
 
 <script>
 import PokemonOptions from '@/components/PokemonOptions.vue'
 import PokemonPicture from '@/components/PokemonPicture.vue'
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+console.log(getPokemonOptions())
 
 export default {
     components: { PokemonOptions, PokemonPicture }
