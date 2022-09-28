9c85e6f6ac455e488903191734f8fd95354da768
Efrain May <efrain_may_75@hotmail.com>
2022-09-28 11:18:11 -0500

    components, pages, styles

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>¿Quien es este Pokemon?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+export default {
+    components: { PokemonOptions, PokemonPicture }
+}
+</script>
+
+<style>
+
+</style>
